dab87b6b6c7141d08c367eff0e605587c019d12d
Fabricio-arjo <faabricio.araujo@gmail.com>
2024-05-05 20:44:40 -0300

    Desafio sintaxe.

--- ContaBanco/src/ContaTerminal.java
+++ ContaBanco/src/ContaTerminal.java
@@ -1,5 +1,36 @@
+import java.util.Locale;
+import java.util.Scanner;
+
 public class ContaTerminal {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+    
+    public static void main(String[] args) {
+      
+      Scanner scanner = new Scanner(System.in).useLocale(Locale.US);
+
+      int numero;
+      String agencia, nomeCliente;
+      double saldo;
+  
+      System.out.println("Informe o número da agência:");
+      agencia = scanner.next();
+     
+      System.out.println("Informe o número da conta: ");
+      numero = scanner.nextInt();
+
+      System.out.println("Informe o nome do cliente:");
+      nomeCliente = scanner.next();
+
+      System.out.println("Informe seu saldo atual:");
+      saldo = scanner.nextDouble();
+
+
+      System.out.println("Olá " + nomeCliente + ", obrigado por criar uma conta em nosso banco, sua agência é "+ agencia + ", conta " + numero  +" e seu saldo " + saldo + " já está disponível para saque.");
+
+
+
+
+
     }
+
+
 }
